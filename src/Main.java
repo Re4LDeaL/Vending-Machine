8597efadfeac2c54c9bf9da0cbd7b1eea0344c53
Re4LDeaL <42698925+Re4LDeaL@users.noreply.github.com>
2018-11-07 15:57:31 +0100

    - Code veranderd and snoepen toegevoegd met scanners

--- src/Main.java
+++ src/Main.java
@@ -4,42 +4,53 @@ public class Main {
 
     public static void main(String[] args) {
 
-      class Snoep
+        Scanner toetsenbord = new Scanner (System.in);
+
+        double Snoep1 = 1.25;
+        double Snoep2 = .75;
+        double Snoep3 = .90;
+        double Snoep4 = .75;
+        double Snoep5 = 1.50;
+        double Snoep6 = .75;
+
+        System.out.print("Kies Snoepnummer aub!");
+        int Snoep = toetsenbord.nextInt();
+
+        System.out.print("De prijs betaald is: ");
+        double betaald = toetsenbord.nextDouble();
+
+        if (Snoep == 2 || Snoep == 4 || Snoep == 6)
         {
-            private int code;
-            private String naam;
-            private double prijs;
-            private int voorraad;
-
-            public Snoep( int code, String naam, double prijs, int voorraad ) {
-                this.code = code;
-                this.naam = naam;
-                this.prijs = prijs;
-                this.voorraad = voorraad;
+            if (betaald >= Snoep2){
+                System.out.println("Bedankt voor uw aankoop!");
+        }
+        if (betaald < Snoep2){
+            System.out.println("U heeft nog " + "€" +(Snoep2-betaald) + " nodig");
+        }
+        }
+        else if (Snoep == 1){
+            if (betaald >= Snoep1){
+                System.out.println("Bedankt voor uw aankoop!");
             }
-
-            public int getCode() {
-                return code;
+            if (betaald < Snoep1)
+            {
+                System.out.println("U heeft nog " + "€" +(Snoep1-betaald) + " nodig");
             }
-
-            public double getPrijs() {
-                return prijs;
+        }
+        else if (Snoep == 3){
+            if (betaald >= Snoep3){
+                System.out.println("Bedankt voor uw aankoop!");
             }
-
-            public String getNaam() {
-                return naam;
+            if (betaald < Snoep3){
+                System.out.println("U heeft nog " + "€" +(Snoep3-betaald) + " nodig");
             }
-
-            public int getVoorraad() {
-                return voorraad;
+        }
+        else if (Snoep == 5){
+            if (betaald >= Snoep5){
+                System.out.println("Bedankt voor uw aankoop!");
             }
-
-            public String toString() {
-                return String.format( "%4d", code ) + ": " +
-                        String.format( "%-40s", naam ) +
-                        "€ " +
-                        String.format( "%6.2f", prijs ) +
-                        String.format( "%4d", voorraad );
+            if (betaald < Snoep5){
+                System.out.println("U heeft nog " + "€" +(Snoep5-betaald) + " nodig");
             }
         }
     }
